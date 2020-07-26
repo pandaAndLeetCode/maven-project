3f72e91c83e7198b239d4cbdd9438e2c85ed2bb9
Roger Alien <abrahamxuacademic@gmail.com>
2020-07-26 19:21:25 -0400

    PLAY WITH MAVEN TESTING

---
+++ server/src/test/java/com/example/MyTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class MyTest {
+
+
+  final String magicWord = "CORRECT";
+  String currentWord = "CORRECT";
+  
+  @Test
+  public void verifyCurrentWord() {
+    assertEquals(magicWord,currentWord);
+  }
+}
